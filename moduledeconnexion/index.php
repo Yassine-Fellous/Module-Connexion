7619c9a1de920feb572b3a68510a16ef3fb08c29
Yassine-Fellous <91788542+Yassine-Fellous@users.noreply.github.com>
2021-11-25 21:19:50 +0100

    module de connexion

---
+++ moduledeconnexion/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/css.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <img class="logo" src="image/devsgame.png" alt="logo">
+        <nav>
+            <ul class="nav_links">
+                <li><a href="index.php">Accueil</a></li>
+                <li><a href="inscription.php">Inscription</a></li>
+                <li><a href="connexion.php">Connexion</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <fieldset class="index">
+            <legend>Presentation</legend><br>
+            <p>DevsGame est un jeux a sens unique en cours de dévellopement 
+            afin d'acceder a la version beta dés sa sorti veuillez crée un compte 
+            c 'est un jeux de type interactive qui premet d'apprendre a develloper
+            tout en s'amusant sans prise de tete et qui s'adaptera a tout les niveaux 
+            et qui jouable meme par un enfant de 8ans ou une personne agé sans prise de tete
+            il vous permettra d'avoir quelque notion </p>
+        </fieldset>
+    </main>
+    <footer>
+    <div class="resaux">
+            <ul class="social-icons">
+                <img class="git" href="" src="github.png" alt="logo">
+            <ul>
+        </div>
+    </footer>
+</body>
+</html>
